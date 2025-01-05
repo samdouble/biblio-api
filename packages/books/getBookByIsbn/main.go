2760f21df697f2c3870806befc505c9a0258043f
samdouble <samleafs@protonmail.com>
2025-01-04 21:53:39 -0500

    ci: set up matrix for every function directory

--- packages/books/getBookByIsbn/main.go
+++ packages/books/getBookByIsbn/main.go
@@ -1,10 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"context"
+    "fmt"
+)
 
-// Hello returns a greeting for the named person.
+type Event struct {
+	Name string `json:"name"`
+}
+
+type Response struct {
+	Body string `json:"body"`
+}
+
+func Main(ctx context.Context, event Event) Response {
+	if event.Name == "" {
+		event.Name = "stranger"
+	}
+	version := ctx.Value("function_version").(string)
+	return Response {
+		Body: "Hello " + event.Name + "! This is function version " + version,
+	}
+}
+
 func main() {
-    // Return a greeting that embeds the name in a message.
-    message := fmt.Sprintf("Hi, %v. Welcome!", "Sam")
-    fmt.Println(message)
+    ctx := context.Background()
+    ctx = context.WithValue(ctx, "function_version", "Test")
+    event := Event{Name: "samdouble"}
+    fmt.Println(Main(ctx, event))
 }
