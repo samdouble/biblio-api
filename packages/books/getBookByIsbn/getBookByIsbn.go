6f1530419617470388cc55a706825800a91ba45f
samdouble <samleafs@protonmail.com>
2025-01-06 20:41:49 -0500

    feat: connect to MongoDB database

--- packages/books/getBookByIsbn/getBookByIsbn.go
+++ packages/books/getBookByIsbn/getBookByIsbn.go
@@ -2,10 +2,21 @@ package main
 
 import (
 	"context"
+	"os"
+    "go.mongodb.org/mongo-driver/v2/mongo"
+    "go.mongodb.org/mongo-driver/v2/mongo/options"
+    "go.mongodb.org/mongo-driver/v2/mongo/readpref"
 	"biblio-api/types"
 )
 
 func Main(ctx context.Context, event types.Event) (types.Response, error) {
+	client, _ := mongo.Connect(options.Client().ApplyURI(os.Getenv("MONGO_URL")))
+	defer func() {
+		if err = client.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+	}()
+
 	if event.Name == "" {
 		event.Name = "stranger"
 	}
